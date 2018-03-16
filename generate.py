ebb2ac87cca3506bcd756c3b78a25c9c94d67a07
ext.Kerry.Zou <ext.kerry.zou@ef.com>
2018-03-16 14:03:03 +0800

    demo

--- generate.py
+++ generate.py
@@ -8,7 +8,7 @@ UPPER = [chr(i) for i in range(0x41, 0x5a)]
 LOWER = [chr(i) for i in range(0x61, 0x7a)]
 LETTER = (NUM + UPPER + LOWER)*2
 
-PIC_SIZE = 120, 30 # width and high
+PIC_SIZE = 120, 40 # width and high
 BACKGROUND_COLOR = 200, 256, 200 # green
 FONT = 'C:/Windows/Fonts/Calibri.ttf'
 FONT_SIZE = 24
@@ -28,15 +28,15 @@ def gene_line(draw, size):
     end = (random.randint(0, width), random.randint(0, height))
     draw.line([begin, end], fill = (0, 0, 0))
 
-def draw():
-    image = Image.new('RGB', PIC_SIZE, BACKGROUND_COLOR)
+def draw(img_size):
+    image = Image.new('RGB', img_size, BACKGROUND_COLOR)
     font = ImageFont.truetype(FONT, FONT_SIZE)
     draw = ImageDraw.Draw(image)
     text = get_text()
     label = ''.join(text)
     for i in range(len(text)):
-        draw.text((i*16, random.randint(0, 10)), text[i], font=font, fill=FONT_COLOR)
-        gene_line(draw, PIC_SIZE)
+        draw.text((i*16, random.randint(0, 12)), text[i], font=font, fill=FONT_COLOR)
+        gene_line(draw, img_size)
     return image, label
 
 def get_index():
@@ -55,19 +55,20 @@ def write_label(filename, label):
     label_file = open('label.txt', 'w')
     label_file.write
 
-def get_verify_pic():
-    if not os.path.isdir(PIC_PATH):
-        os.makedirs(PIC_PATH)
-    index = get_index() + 1
-    # label_file = open('label.txt', 'a')
-    for i in range(index, SAMPLES + index):
-        image, label = draw()
-        pic_name = str(i) + SUFFIX
-        print(np.float32(image).shape)
-        # image.save(PIC_PATH + pic_name, 'png')
-        # label_file.write(pic_name + ':' + label + '\n')
-    # label_file.close()
+# def get_verify_pic():
+#     if not os.path.isdir(PIC_PATH):
+#         os.makedirs(PIC_PATH)
+#     index = get_index() + 1
+#     return draw()
+#     label_file = open('label.txt', 'a')
+#     for i in range(index, SAMPLES + index):
+#         image, label = draw()
+#         pic_name = str(i) + SUFFIX
+#         print(np.float32(image).shape)
+#         image.save(PIC_PATH + pic_name, 'png')
+#         label_file.write(pic_name + ':' + label + '\n')
+#     label_file.close()
 
 
-if __name__ == '__main__':
-    get_verify_pic()
+# if __name__ == '__main__':
+#     get_verify_pic()
--- train.py
+++ train.py
@@ -1,5 +1,100 @@
 import cv2
 import numpy as np
+import generate
 from keras.layers import *
 from keras.models import Model
+from keras.optimizers import SGD
+from keras import backend as K
+
+CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+CHAR_DICT = {c:i for i, c in enumerate(CHARS)}
+NUM_CHAR = len(CHARS)
+
+def build_model():
+    inputs = Input((None, 40, 1), name='inputs')
+    x = inputs
+    base_conv = 32
+    for i in range(3):
+        x = Conv2D(base_conv * (2**i), 3, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = MaxPooling2D()(x)
+    x = Conv2D(256, 5)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(512, 1)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    pred = Conv2D(NUM_CHAR + 1, 1, activation='softmax')(x)
+    return inputs, pred
+
+def encode(max_char, text):
+    t = np.zeros((max_char,))
+    for i, c in enumerate(text):
+        t[i] = CHAR_DICT[c]
+    if len(text) < max_char:
+        t[len(text):max_char] = NUM_CHAR
+    return t
+
+class ImageGenerator:
+    def __init__(self, img_size, num_examples, batch_size, max_len=6):
+        self.num_examples = num_examples
+        self.batch_size = batch_size
+        self.max_len = max_len
+        self.img_h, self.img_w = img_size
+        self.input_len = self.img_w // 8 - 4
+
+    def next_batch(self):
+        labels = np.zeros((self.batch_size, self.max_len))
+        images = np.zeros((self.batch_size, self.img_w, self.img_h, 1))
+        for i in range(self.batch_size):
+            image, text = generate.draw((self.img_w, self.img_h))
+            label = encode(self.max_len, text)
+            labels[i] = label
+            image = np.asarray(image)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            image = cv2.resize(image, (self.img_h, self.img_w))
+            image = np.reshape(image, (self.img_w, self.img_h, 1))
+            images[i, ...] = image
+        input_length = np.zeros((self.batch_size, 1))
+        label_length = np.zeros((self.batch_size, 1))
+        input_length[:] = self.input_len
+        label_length[:] = self.max_len
+        inputs = {
+            'inputs':images,
+            'labels':labels,
+            'input_length':input_length,
+            'label_length':label_length
+        }
+        outputs = {'ctc':np.zeros((self.batch_size,))}
+        return inputs, outputs
+    
+    def get_data(self):
+        while True:
+            yield self.next_batch()
+
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    y_pred = y_pred[:, :, 0, :]
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+if __name__ == '__main__':
+    inputs, y_pred = build_model()
+    labels = Input((6,), dtype='float32', name='labels')
+    input_len = Input((1,), dtype='int64', name='input_length')
+    label_len = Input((1,), dtype='int64', name='label_length')
+    ctc_loss = Lambda(ctc_lambda_func, (1,), name='ctc')([y_pred, labels, input_len, label_len])
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
+    model = Model(inputs=[inputs, labels, input_len, label_len], outputs=[ctc_loss])
+    model.compile(loss={'ctc':lambda y_true, y_pred: y_pred}, optimizer=sgd)
+    train = ImageGenerator((40,120), 1280, 64)
+    test = ImageGenerator((40,120), 640, 64)
+    model.fit_generator(
+        generator=train.get_data(),
+        steps_per_epoch=train.num_examples / train.batch_size,
+        epochs=1,
+        validation_data=test.get_data(),
+        validation_steps=test.num_examples / test.batch_size
+    )
 
